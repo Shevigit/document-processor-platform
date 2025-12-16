c36046a3b1651fd944ed9dbed8aa53b15cd165a5
לבורנטית לבורנטית <@smg.local>
2025-12-16 12:34:28 +0200

    add css to the home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,18 +1,32 @@
-﻿import { Link } from "react-router-dom";
+﻿import { Box, Container, Paper, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div style={{ padding: 16 }}>
-      <h1>Document Ingestion - Home</h1>
+    <Container maxWidth="sm">
+        <Paper sx={{ p:20, mt: 20 }}>
+      <Box sx={{ py: 4}}>
+    <div style={{ padding: 16}} >
+       <Typography variant="h5" component="h1" gutterBottom align="center">Document Ingestion - Home</Typography>
+
       <ul>
         <li>
-          <Link to="/upload">דף העלאה</Link>
+           <Typography variant="body1">
+           <Link to="/upload">דף העלאה</Link>     
+           </Typography>
+        
         </li>
         <li>
-          <Link to="/documents">רשימת מסמכים</Link>
+        <Typography variant="body1">
+        <Link to="/documents">רשימת מסמכים</Link>
+           </Typography>
+          
         </li>
       </ul>
     </div>
+    </Box>
+    </Paper>
+    </Container>
   );
 };
 
